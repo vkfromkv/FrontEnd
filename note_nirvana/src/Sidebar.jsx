8f291e2c1d364538eb975102462d6c7d3d6a097c
Laraib Fatima <fatima.laraib775@gmail.com>
2023-10-21 23:31:32 -0400

    Generic Layout

---
+++ note_nirvana/src/Sidebar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <nav className="col-md-3 col-lg-2 d-md-block bg-light sidebar">
+      <div className="position-sticky">
+        <>
+          <div className="top-element">
+            <p className="top-element-text">Note Nirvana</p>
+          </div>
+          <ul className="nav flex-column" style={{ marginTop: "20px" }}>
+            <li className="nav-item">
+              <a className="nav-link active" href="#">
+                Sign Up
+              </a>
+            </li>
+            <li className="nav-item">
+              <div className="spacer"></div>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Login
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Forums
+              </a>
+            </li>
+            {/* Add more sidebar items as needed */}
+          </ul>
+        </>
+      </div>
+    </nav>
+  );
+}
+
+export default Sidebar;
