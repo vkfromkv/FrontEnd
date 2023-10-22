b2356d9b51c44b69ae625f720875f56877c0aaf8
Laraib Fatima <fatima.laraib775@gmail.com>
2023-10-22 16:26:14 -0400

    Added about us page

--- note_nirvana/src/Sidebar.jsx
+++ note_nirvana/src/Sidebar.jsx
@@ -5,31 +5,32 @@ function Sidebar() {
   return (
     <nav className="col-md-3 col-lg-2 d-md-block bg-light sidebar">
       <div className="position-sticky">
-        <div className="top-element">
-          <Link to="/" className="top-element-text note-nirvana-link">
-            Note Nirvana
+        <>
+        
+          <Link to="/" className="top-element non-link">
+            <p className="top-element-text">Note Nirvana</p>
           </Link>
-        </div>
-        <ul className="nav flex-column" style={{ marginTop: "20px" }}>
-          <li className="nav-item">
-            <Link to="/about" className="nav-link active">
-              Sign Up
-            </Link>
-          </li>
-          <li className="nav-item">
-            <div className="spacer"></div>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Login
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Forums
-            </a>
-          </li>
-        </ul>
+          <ul className="nav flex-column" style={{ marginTop: "20px" }}>
+            <li className="nav-item">
+              <a className="nav-link active" href="#">
+                Sign Up
+              </a>
+            </li>
+            <li className="nav-item">
+              <div className="spacer"></div>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Login
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Forums
+              </a>
+            </li>
+          </ul>
+        </>
       </div>
     </nav>
   );
