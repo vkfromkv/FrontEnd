c6d6b9721b29bffc74d4c7efb4b38775e80177f0
Laraib Fatima <fatima.laraib775@gmail.com>
2023-10-22 15:33:39 -0400

    Added about us page

--- note_nirvana/src/Sidebar.jsx
+++ note_nirvana/src/Sidebar.jsx
@@ -1,34 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom"; 
 
 function Sidebar() {
   return (
     <nav className="col-md-3 col-lg-2 d-md-block bg-light sidebar">
       <div className="position-sticky">
-        <>
-          <div className="top-element">
-            <p className="top-element-text">Note Nirvana</p>
-          </div>
-          <ul className="nav flex-column" style={{ marginTop: "20px" }}>
-            <li className="nav-item">
-              <a className="nav-link active" href="#">
-                Sign Up
-              </a>
-            </li>
-            <li className="nav-item">
-              <div className="spacer"></div>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Login
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Forums
-              </a>
-            </li>
-          </ul>
-        </>
+        <div className="top-element">
+          <Link to="/" className="top-element-text note-nirvana-link">
+            Note Nirvana
+          </Link>
+        </div>
+        <ul className="nav flex-column" style={{ marginTop: "20px" }}>
+          <li className="nav-item">
+            <Link to="/about" className="nav-link active">
+              Sign Up
+            </Link>
+          </li>
+          <li className="nav-item">
+            <div className="spacer"></div>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Login
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Forums
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
