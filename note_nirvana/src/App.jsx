c690c76935b39073b4c6e52426f33f551b77b6d7
Nikhil Pittala <nikhilpittala7@gmail.com>
2023-12-01 14:13:02 -0500

    Publish tab done

--- note_nirvana/src/App.jsx
+++ note_nirvana/src/App.jsx
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import RootLayout from "../Pages/RootLayout";
-import About from "../Pages/About";
+import RootLayout from "./Pages/RootLayout";
+import About from "./Pages/About";
+import Publish from "./Pages/Publish";
+import { action as formAction } from "./Pages/Publish";
 function App() {
   const [count, setCount] = useState(0);
   const router = createBrowserRouter([
     {
       path: "/",
       element: <RootLayout></RootLayout>,
-      children: [{ path: "/about", element: <About></About> }],
+      children: [
+        { path: "/about", element: <About></About> },
+        { path: "/publish", element: <Publish></Publish>, action: formAction },
+      ],
     },
   ]);
   return <RouterProvider router={router}></RouterProvider>;
---
+++ note_nirvana/src/Pages/RootLayout.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Bars/Navbar";
+import Sidebar from "../components/Bars/Sidebar";
+import UserSideBar from "../components/Bars/userSideBar";
+import AuthContext from "../components/userLogin/UserContext";
+
+const RootLayout = () => {
+  const [hasAccount, setDoesnot] = useState(true);
+  const [userEmail, setEmail] = useState("nikhilpittala7");
+  const [LoggedIn, setLoggedIn] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
+
+  return (
+    <>
+      <AuthContext.Provider
+        value={{
+          LoggedIn: LoggedIn,
+          setLoggedIn: setLoggedIn,
+          hasAccount: hasAccount,
+          setDoesnot: setDoesnot,
+          openModal: openModal,
+          setOpenModal: setOpenModal,
+          userEmail: userEmail,
+          setEmail: setEmail,
+        }}
+      >
+        <div
+          style={{ display: "flex", flexDirection: "column", height: "100vh" }}
+        >
+          <div>
+            <Navbar />
+          </div>
+          <div style={{ display: "flex", flex: 1 }}>
+            <UserSideBar></UserSideBar>
+            <main className="main-content col-md-10.5 p-5" style={{ flex: 1 }}>
+              <Outlet></Outlet>
+            </main>
+          </div>
+        </div>
+      </AuthContext.Provider>
+    </>
+  );
+};
+
+export default RootLayout;
