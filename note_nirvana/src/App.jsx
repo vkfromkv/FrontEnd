078c20463c6fbf67d827b3b2f7bbf7c93c5056a5
Laraib Fatima <fatima.laraib775@gmail.com>
2023-11-30 19:16:24 -0500

    Added contact page

--- note_nirvana/src/App.jsx
+++ note_nirvana/src/App.jsx
@@ -2,13 +2,18 @@ import React, { useState } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "../Pages/RootLayout";
 import About from "../Pages/About";
+import ContactUs from "../Pages/ContactUs";
+
+
 function App() {
   const [count, setCount] = useState(0);
   const router = createBrowserRouter([
     {
       path: "/",
       element: <RootLayout></RootLayout>,
-      children: [{ path: "/about", element: <About></About> }],
+      children: [{ path: "/about", element: <About></About> },
+      { path: "/contactus", element: <ContactUs></ContactUs> },
+    ],
     },
   ]);
   return <RouterProvider router={router}></RouterProvider>;
