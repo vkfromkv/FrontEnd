52eeece15a80632223e798783796174c34f3b729
Laraib Fatima <fatima.laraib775@gmail.com>
2023-12-01 23:52:36 -0500

    Added search bar and fixed fluid layout

--- note_nirvana/src/components/Bars/Sidebar.jsx
+++ note_nirvana/src/components/Bars/Sidebar.jsx
@@ -1,56 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
+import "./Sidebar.css";
 
 import User from "../userLogin/User";
 
 import AuthContext from "../userLogin/UserContext";
 
 function Sidebar() {
-  const [hasAccount, setDoesnot] = useState(true);
-  const [LoggedIn, setLoggedIn] = useState(false);
-  const [openModal, setOpenModal] = useState(false);
+  const ctx = useContext(AuthContext);
 
   return (
-    <AuthContext.Provider
-      value={{
-        LoggedIn: LoggedIn,
-        setLoggedIn: setLoggedIn,
-        hasAccount: hasAccount,
-        setDoesnot: setDoesnot,
-      }}
-    >
-      <nav
-        className="col-md-2 d-none d-md-block bg-light sidebar  "
-        style={{ maxWidth: "180px" }}
-      >
-        <div className="sidebar-sticky">
-          <ul className="nav flex-column p-2 pt-4">
-            <li className="nav-item ">
-              <button
-                className="btn btn-primary w-100"
-                type="submit"
-                onClick={() => {
-                  setOpenModal(true);
-                }}
-              >
-                Sign In
-              </button>
-              {openModal && (
-                <User closeModal={setOpenModal} hasAccount={hasAccount}></User>
-              )}
-            </li>
-            <li className="nav-item">
-              <div className="spacer"></div>
-            </li>
-            <li className="nav-item text-center">
-              <a className="nav-link" href="#">
-                Forums
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </AuthContext.Provider>
+    <nav className="sidebar custom-sidebar d-none d-md-block bg-light">
+      <div className="sidebar-sticky">
+        <ul className="nav flex-column  pt-4">
+          <li className="nav-item ">
+            <button
+              className="btn btn-primary w-100"
+              type="submit"
+              onClick={() => {
+                ctx.setOpenModal(true);
+              }}
+            >
+              Sign In
+            </button>
+            {ctx.openModal && <User></User>}
+          </li>
+          <li className="nav-item">
+            <div className="spacer"></div>
+          </li>
+          <li className="nav-item text-center">
+            <a className="nav-link" href="#">
+              Forums
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
